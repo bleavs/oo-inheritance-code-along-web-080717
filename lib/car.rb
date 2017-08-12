aaa739c0ca6ff768fccb45108a551abde5c8de69
bleavs <bleavitt@colgate.edu>
2017-08-12 09:37:22 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,9 @@
-require_relative "./vehicle.rb"
+require_relative "./vehicle.rb" #gives us access to vehicle class file
+
+class Car < Vehicle #class car inherits from Vehicle class
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+ end
+
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,23 @@
-class Vehicle
+class Vehicle #parent class or super class
+
+#defining methods so subclasses,
+#when created will have access to parent methods
+
+attr_accessor :wheel_size, :wheel_number
+
+def initialize(wheel_size, wheel_number)
+
+  @wheel_size = wheel_size
+
+  @wheel_number = wheel_number
+end
+
+def go
+  "vrrrrrrrooom!"
+end
+
+def fill_up_tank
+  "filling up!"
+end
 
 end
